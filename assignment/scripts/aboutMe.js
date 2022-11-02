9afeb6ed4afd609425a06cce71ac43797f3bce23
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-11-02 10:23:15 -0500

    created more variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -24,11 +24,19 @@ let luckyNumber = 9
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 
+console.log( "My name is" (fullName), "and I think" (luckyNumber), "is a winner!" );
+
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+
+let adventurous = true;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
+let food = ( 'tacos', 'mango', 'sushi' );
+
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+
+let pets = 2; 
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 
